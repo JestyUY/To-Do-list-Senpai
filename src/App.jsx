2de9abb74cb3f51414ad7d66cf626905b62ce0d3
JestyUY <104555840+JestyUY@users.noreply.github.com>
2023-07-01 20:01:42 +1200

    Button component added with Dark mode functionality

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+
+// import { useEffect, useState } from 'react'
+import './App.css'
+import ButtonMode from './ButtonMode/ButtonMode';
+
+function App() {
+
+
+  return (
+   <div className='bg-Mobile-Light w-screen h-screen bg-no-repeat bg-contain dark:bg-Mobile-Dark lg:bg-Desktop-Light dark:lg:bg-Desktop-Dark  ' >
+    <ButtonMode />
+    
+
+   </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode:'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    colors:{},
+    extend: { 
+      backgroundImage: {
+      'Mobile-Light': "url('./src/assets/bg-mobile-light.jpg')",
+      'Mobile-Dark': "url('./src/assets/bg-mobile-dark.jpg')",
+      'Desktop-Light': "url('./src/assets/bg-desktop-light.jpg')",
+      'Desktop-Dark': "url('./src/assets/bg-desktop-dark.jpg')"
+    }},
+  },
+  plugins: [],
+  
+}
+
