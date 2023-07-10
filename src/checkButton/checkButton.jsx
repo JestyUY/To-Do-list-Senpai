8d90542f27db963f9a3f887c00becd0b7baec153
JestyUY <104555840+JestyUY@users.noreply.github.com>
2023-07-10 13:32:46 +1200

    updates on CheckButton to change isCompleted value.

--- src/checkButton/checkButton.jsx
+++ src/checkButton/checkButton.jsx
@@ -1,10 +1,13 @@
 const checkLogo = <svg className="" xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FaFaFa" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
-function CheckButton () {
+function CheckButton ({gradient,onClick }) {
+     
 
+  
+        
     
-
+    
     return(
-        <button className=" my-auto scale-75 h-9 w-9 items-center flex justify-center rounded-full bg-gradient-to-br from-brightBlue to-pinkGrad ">{checkLogo}</button>
+        <button onClick={onClick} className={` my-auto scale-75 h-9 w-9 items-center flex justify-center rounded-full   ${gradient}  `}>{checkLogo}</button>
     )
 }
 
--- src/input/input.jsx
+++ src/input/input.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react"
 
-import { useState } from "react";
+import {  useState } from "react";
 
 import CheckButton from "../checkButton/checkButton";
 
@@ -15,52 +15,71 @@ function Input() {
       isCompleted: false,
     },
   ]);
-//   const [inputArrayRev, setInputArrayRev] = useState([
-//     {
-//       key: 2,
-//       task: "Finalizar práctica de estados y props",
-//       isCompleted: false,
-//     },
-//     { key: 1, task: "Tomar agua", isCompleted: true },
-//   ]);
+  //   const [inputArrayRev, setInputArrayRev] = useState([
+  //     {
+  //       key: 2,
+  //       task: "Finalizar práctica de estados y props",
+  //       isCompleted: false,
+  //     },
+  //     { key: 1, task: "Tomar agua", isCompleted: true },
+  //   ]);
 
   function inputClick(ev) {
-    const lastValueOfId = [...inputArray].pop().key + 1;
+    // const lastValueOfId = [...inputArray].pop().key + 1;
+    const lastValueOfId = inputArray[inputArray.length - 1].key + 1
     ev.preventDefault();
-
+    
     setInputArray([
       ...inputArray,
       { key: lastValueOfId, task: ev.target.value, isCompleted: false },
     ]);
     // setInputArrayRev(inputArray.reverse());
   }
+  const newInputArray = [...inputArray]
+  function changingStatus(index) {
+newInputArray[index].isCompleted = !newInputArray[index].isCompleted;
+setInputArray(newInputArray)
+  }
+  
+  
 
- 
   return (
     <>
       <input
         type="text"
-        className=" w-full h-14 rounded-lg text-center bg-veryLightGray dark:bg-VeryDarkDesaturatedBlueDM"
+        className="dark:text-LightGrayishBlueDM  text-veryDarkGrayishBlue w-full h-14 rounded-lg text-center bg-veryLightGray dark:bg-VeryDarkDesaturatedBlueDM"
         onClick={inputClick}
       />
-      <ul className="dark:[&>li]:text-LightGrayishBlueDM [&>li]:text-veryDarkGrayishBlue shadow-2xl max-h-96 drop-shadow-2xl overflow-y-scroll [&>li]:flex  no-scrollbar w-full items-center flex flex-col  mt-8 dark:[&>li]:bg-VeryDarkDesaturatedBlueDM [&>li]:bg-veryLightGrayish [&>li]:w-full [&>li]:h-16 [&>li]: [&>li]:pl-8 [&>li]:content-center   [&>li:first-child]:rounded-t-xl [&>li:last-child]:rounded-b-xl divide-y-[1px] divide-lightGrayishBlue dark:divide-VeryDarkGrayishBlueDM">
-
-        { [...inputArray].reverse().map((ob, index) => (
-          <li key={index}
+      <ul className="dark:[&>li]:text-LightGrayishBlueDM [&>li]:text-veryDarkGrayishBlue shadow-2xl max-h-96 drop-shadow-2xl overflow-y-scroll [&>li]:flex  no-scrollbar w-full items-center flex flex-col  mt-8 dark:[&>li]:bg-VeryDarkDesaturatedBlueDM [&>li]:bg-veryLightGrayish [&>li]:w-full [&>li]:h-16 [&>li]: [&>li]:pl-8 [&>li]:content-center   [&>li:first-child]:rounded-t-xl  divide-y-[1px] divide-lightGrayishBlue dark:divide-VeryDarkGrayishBlueDM">
+        {newInputArray.sort((a, b) => b.key - a.key).map((ob, index) => (
+          <li
+            key={index}
             className={
               ob.isCompleted
                 ? "underline text-gray-400"
                 : "no-underline text-black shrink-0"
             }
           >
-            <CheckButton /> <span className="my-auto mx-2">{ob.task}</span> 
+            <CheckButton onClick={changingStatus(index)} gradient={ob.isCompleted ? 'bg-gradient-to-br from-brightBlue to-pinkGrad':''}/> <span className="my-auto mx-2">{ob.task}</span>
           </li>
         ))}
       </ul>
-      <ul className=" shadow-2xl  drop-shadow-2xl dark:[&>li]:bg-VeryDarkDesaturatedBlueDM  w-full items-center flex flex-col   [&>li]:bg-veryLightGrayish [&>li]:w-full [&>li]:h-14 [&>li]: [&>li]:pl-8 [&>li]:flex [&>li]:flex-col [&>li]:justify-center  [&>li:last-child]:rounded-b-xl border-t-[1px] border-lightGrayishBlue dark:border-VeryDarkGrayishBlueDM rounded-b-lg">
-        <li className="">Here</li>
-      </ul>
-    </>
+      
+
+      <div className="text-veryDarkGrayishBlue dark:text-LightGrayishBlueDM shadow-2xl  drop-shadow-2xl dark:bg-VeryDarkDesaturatedBlueDM  items-center   bg-veryLightGrayish w-full h-28  :flex-col   [last-child]:rounded-b-xl border-t-[1px] border-lightGrayishBlue dark:border-VeryDarkGrayishBlueDM rounded-b-lg grid grid-cols-12 grid-rows-2 gap-4">
+        <span className="col-span-8">Counter</span>
+        <div className="row-start-2 col-span-12 border-t h-full w-full">
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </div>
+        <div className="col-start-9 col-span-4 flex">
+          <button>Clear</button>
+          <button>Completed</button>
+        </div>
+      </div>
+      </>
+    
   );
 }
 
