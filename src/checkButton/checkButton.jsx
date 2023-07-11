f32148098f887abccc384106676d47376d7fffe6
JestyUY <104555840+JestyUY@users.noreply.github.com>
2023-07-12 11:57:48 +1200

    updates on buttons, app nearly finish

--- src/checkButton/checkButton.jsx
+++ src/checkButton/checkButton.jsx
@@ -1,16 +1,13 @@
-const checkLogo = <svg className="" xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FaFaFa" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
 // eslint-disable-next-line react/prop-types
-function CheckButton ({gradient,onClick }) {
-     
-
-  
-        
-    
-    
-    return(
-        <button onClick={onClick} className={` border border-lightGrayishBlue hover:border-veryDarkGrayishBlue  my-auto scale-75 h-9 w-9 items-center flex justify-center rounded-full   ${gradient}  `}>{checkLogo}</button>
-    )
+function CheckButton({ gradient, onClick, logo }) {
+  return (
+    <button
+      onClick={onClick}
+      className={` border border-lightGrayishBlue hover:border-veryDarkGrayishBlue  my-auto scale-75 h-9 w-9 items-center flex justify-center rounded-full   ${gradient}  `}
+    >
+      {logo}
+    </button>
+  );
 }
 
-
-export default CheckButton
+export default CheckButton;
--- src/input/input.jsx
+++ src/input/input.jsx
@@ -1,11 +1,21 @@
 // import { useState } from "react"
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import CheckButton from "../checkButton/checkButton";
+const checkLogo = (
+  <svg className="" xmlns="http://www.w3.org/2000/svg" width="11" height="9">
+    <path
+      fill="none"
+      stroke="#FaFaFa"
+      strokeWidth="2"
+      d="M1 4.304L3.696 7l6-6"
+    />
+  </svg>
+);
 
 function Input() {
-  // const [initial, setInitial] = useState('')
+  const [initial, setInitial] = useState("");
   // const [completed, setCompleted] = useState(false)
   const [inputArray, setInputArray] = useState([
     { key: 1, task: "Tomar agua", isCompleted: true },
@@ -33,46 +43,53 @@ function Input() {
         ...inputArray,
       ];
       setInputArray(newValueOfArrays);
-
-      setNewInputArray(newValueOfArrays);
-      console.log(inputArray, newInputArray);
+      setInitial("");
     }
   }
 
-  const [newInputArray, setNewInputArray] = useState([...inputArray]);
+  const [filter, setFilter] = useState("all"); // all || completed || active
+
+  let visibleElements = inputArray;
+  if (filter === "completed") {
+    visibleElements = inputArray.filter((tarea) => tarea.isCompleted);
+  } else if (filter === "active") {
+    visibleElements = inputArray.filter((tarea) => !tarea.isCompleted);
+  }
   function changingStatus(index) {
-    const newArray = [...newInputArray];
+    const newArray = [...inputArray];
     newArray[index].isCompleted = !newArray[index].isCompleted;
-    setNewInputArray(newArray);
+    setInputArray(newArray);
   }
 
   function activeTask() {
-    const activeArray = inputArray.filter((ob) => ob.isCompleted === false);
-    setNewInputArray(activeArray);
+    setFilter("active");
   }
 
   function allTask() {
-    setNewInputArray(inputArray);
+    setFilter("all");
   }
   function completedTask() {
-    const completedArray = inputArray.filter((ob) => ob.isCompleted === true);
-    setNewInputArray(completedArray);
+    setFilter("completed");
   }
 
-  const [taskLeft, setTaskLeft] = useState(0);
-
-  useEffect(() => {
-    const taskLeftArray = newInputArray.filter(
-      (ob) => ob.isCompleted === false
-    );
-    setTaskLeft(taskLeftArray.length);
-  }, [newInputArray]);
+  let taskLeft = 0;
+  if (filter === "active") {
+    taskLeft = visibleElements.length;
+  } else if (filter === "completed") {
+    taskLeft = inputArray.length - visibleElements.length;
+  } else {
+    taskLeft = inputArray.filter((task) => !task.isCompleted).length;
+  }
 
   function clear() {
-    [...newInputArray].map((ob) => {
+    const newState = [...inputArray].map((ob) => {
       ob.isCompleted = false;
       return ob;
-    }, setInputArray(newInputArray));
+    });
+    setInputArray(newState);
+    if (filter === "completed") {
+      setFilter("all");
+    }
   }
 
   return (
@@ -81,14 +98,16 @@ function Input() {
         type="text"
         className="dark:text-LightGrayishBlueDM  text-veryDarkGrayishBlue w-full h-14 rounded-lg text-center bg-veryLightGray dark:bg-VeryDarkDesaturatedBlueDM"
         onKeyDown={inputClick}
+        onChange={(ev) => setInitial(ev.target.value)}
+        value={initial}
       />
       <ul className="dark:[&>li]:text-LightGrayishBlueDM [&>li]:text-veryDarkGrayishBlue shadow-2xl max-h-96 drop-shadow-2xl overflow-y-scroll [&>li]:flex  no-scrollbar w-full items-center flex flex-col  mt-8 dark:[&>li]:bg-VeryDarkDesaturatedBlueDM [&>li]:bg-veryLightGrayish [&>li]:w-full [&>li]:h-16 [&>li]: [&>li]:pl-8 [&>li]:content-center   [&>li:first-child]:rounded-t-xl  divide-y-[1px] divide-lightGrayishBlue dark:divide-VeryDarkGrayishBlueDM">
-        {newInputArray.map((ob, index) => (
+        {visibleElements.map((ob, index) => (
           <li
             key={index}
             className={
               ob.isCompleted
-                ? "line-through text-gray-400"
+                ? "line-through text-gray-400 shrink-0"
                 : "no-underline text-black shrink-0"
             }
           >
@@ -99,8 +118,24 @@ function Input() {
                   ? "bg-gradient-to-br from-brightBlue to-pinkGrad"
                   : ""
               }
+              logo={ob.isCompleted ? checkLogo : ""}
             />{" "}
             <span className="my-auto mx-2">{ob.task}</span>
+            <button
+              className=""
+              onClick={() => {
+                const newArray = inputArray.filter(
+                  (item) => item.key !== ob.key
+                );
+                console.log(newArray);
+
+                console.log(ob);
+                setInputArray(newArray);
+              }}
+            >
+              {" "}
+              X
+            </button>
           </li>
         ))}
       </ul>
@@ -108,13 +143,22 @@ function Input() {
       <div className="text-veryDarkGrayishBlue dark:text-LightGrayishBlueDM shadow-2xl  drop-shadow-2xl dark:bg-VeryDarkDesaturatedBlueDM  items-center   bg-veryLightGrayish w-full h-16  flex  [last-child]:rounded-b-xl border-t-[1px] border-lightGrayishBlue dark:border-VeryDarkGrayishBlueDM rounded-b-lg justify-evenly gap-4">
         <span className="ml-2"> {`${taskLeft} left`}</span>
         <div className="md:flex h-full justify-between grow max-w-xs hidden">
-          <button className="" onClick={allTask}>
+          <button
+            className={filter === "all" ? "font-black " : ""}
+            onClick={allTask}
+          >
             All
           </button>
-          <button className="" onClick={activeTask}>
+          <button
+            className={filter === "active" ? "font-black " : ""}
+            onClick={activeTask}
+          >
             Active
           </button>
-          <button className="" onClick={completedTask}>
+          <button
+            className={filter === "completed" ? "font-black " : ""}
+            onClick={completedTask}
+          >
             Completed
           </button>
         </div>
@@ -127,13 +171,22 @@ function Input() {
       </div>
 
       <div className=" mt-5 grow md:hidden  text-veryDarkGrayishBlue dark:text-LightGrayishBlueDM shadow-2xl  drop-shadow-2xl dark:bg-VeryDarkDesaturatedBlueDM  items-center  bg-veryLightGrayish w-full h-16  flex  border-t-[1px] border-lightGrayishBlue dark:border-VeryDarkGrayishBlueDM rounded-lg justify-evenly ">
-        <button className="" onClick={allTask}>
+        <button
+          className={filter === "all" ? "font-black " : ""}
+          onClick={allTask}
+        >
           All
         </button>
-        <button className="" onClick={activeTask}>
+        <button
+          className={filter === "active" ? "font-black " : ""}
+          onClick={activeTask}
+        >
           Active
         </button>
-        <button className="" onClick={completedTask}>
+        <button
+          className={filter === "completed" ? "font-black " : ""}
+          onClick={completedTask}
+        >
           Completed
         </button>
       </div>
